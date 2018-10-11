05264839eab6bba1b77fab7f7c61836c27fe5e5c
zhangye <zhangye98@foxmail.com>
2018-10-11 19:35:25 +0800

    (测试提交）

--- app/src/main/java/com/shoumei/xhn/archives/data/ArchivesType.kt
+++ app/src/main/java/com/shoumei/xhn/archives/data/ArchivesType.kt
@@ -5,12 +5,12 @@ package com.shoumei.xhn.archives.data
  * Created by zhangye on 2018/9/20.
  */
 object ArchivesType {
-    val AVATAR = "avatar" //头像
+    const val AVATAR = "avatar" //头像
     val BIRTH = "birth" //生日
-    val GENDER = "gender" //性别
+    const val GENDER = "gender" //性别
     val INDUSTRY = "industry" //所属行业
-    val ADDRESS = "address" //地址
-    val PROVINCE = "province" //省份
-    val CITY = "city" //城市
-    val TOWN = "town" //区县
+    const val ADDRESS = "address" //地址
+    const val PROVINCE = "province" //省份
+    const val CITY = "city" //城市
+    const val TOWN = "town" //区县
 }
